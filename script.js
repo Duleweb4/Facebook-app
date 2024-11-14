69b70f0e668f1a0ff9d9140d18fa8df5bdc20874
Duleweb4 <161235466+Duleweb4@users.noreply.github.com>
2024-11-14 23:38:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector(".dugme").addEventListener( 'onclick', myFunction)
+{
+    function  myFunction ()
+    {
+        console.log("Ulogovali ste se");
+    }
+    myFunction();
+
+}
